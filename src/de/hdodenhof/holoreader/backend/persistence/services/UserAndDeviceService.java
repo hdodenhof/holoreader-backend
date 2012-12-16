739a1627876e0f7feda10279837aeddd02aed3e2
Henning Dodenhof <henning.dodenhof@gmail.com>
2012-12-16 15:41:07 +0100

    Send activation email when account has been created from device api

--- src/de/hdodenhof/holoreader/backend/persistence/services/UserAndDeviceService.java
+++ src/de/hdodenhof/holoreader/backend/persistence/services/UserAndDeviceService.java
@@ -1,5 +1,14 @@
 package de.hdodenhof.holoreader.backend.persistence.services;
 
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 
@@ -16,6 +25,7 @@ public class UserAndDeviceService {
         if (user == null) {
             user = new UserEntity();
             user.seteMail(eMail);
+            notifyAccountActivation(user);
         }
 
         DeviceDao deviceDao = new DeviceDao();
@@ -38,6 +48,26 @@ public class UserAndDeviceService {
         UserDao userDao = new UserDao();
         userDao.persist(user);
         return user;
+    }
+
+    private void notifyAccountActivation(UserEntity user) {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        String msgBody = "Hello!\n\nHolo Reader FeedToDevice has been enabled for your account.\n"
+                + "You can now go to https://holoreader.appspot.com and send feeds to your device(s).\n\n"
+                + "Please contact holoreader@hdodenhof.de if you have any issue.\n\n" + "Thanks\n" + "Henning";
+
+        try {
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress("noreply@holoreader.appspotmail.com", "Holo Reader"));
+            msg.setReplyTo(new Address[] { new InternetAddress("holoreader@hdodenhof.de") });
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(user.geteMail()));
+            msg.setSubject("Holo Reader FeedToDevice");
+            msg.setText(msgBody);
+            Transport.send(msg);
+        } catch (Exception e) {
+        }
     }
 
     public UserEntity get(String eMail) {
